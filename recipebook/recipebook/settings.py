5b22cabb56f4a228f4a3e8ace827c79676b71d3e
skyant83 <skyant83@gmail.com>
2025-03-24 17:31:33 +0800

    Created a main css file - Moved all css stylings to its own css file
    Modified Base Template
    - Included stylesheet link to the main css file
    - Replaced old manual css stylings with the respective css classes
    
    Modified Page Templates
    - Included the django static files load function
    
    Modified Recipe Detail Template
    - Replaced manual styling with css classes
    - Displays related recipe image and provides its description as an alt text discription

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATICFILE_DIRS = [BASE_DIR/'static']
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
